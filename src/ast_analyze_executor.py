61d175f48429f9b5e7f4ff29cccf5a3d20694dd3
Nishiura <k-nishiura@aist.go.jp>
2019-01-22 18:38:41 +0900

    first commit

---
+++ src/ast_analyze_executor.py
@@ -0,0 +1,13 @@
+import logging.config
+from ast.ast_processor import AstProcessor
+from ast.basic_info_listener import BasicInfoListener
+
+
+if __name__ == '__main__':
+    logging_setting_path = '../resources/logging/utiltools_log.conf'
+    logging.config.fileConfig(logging_setting_path)
+    logger = logging.getLogger(__file__)
+
+    target_file_path = '/Users/k-nishiura/JavaPerser/TourInfoServiceImpl.java'
+
+    ast_info = AstProcessor(logging, BasicInfoListener()).execute(target_file_path)
